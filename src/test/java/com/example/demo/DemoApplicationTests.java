dc4b6b58f02693f2a5047a3c2718f2a1d460ad09
codersergg <sergey.i.chopin@gmail.com>
2021-09-19 14:19:34 +0300

    without test fail 2

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -7,8 +7,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-		//Assertions.fail("Oooooopppppps!!!");
-	}
+    @Test
+    void contextLoads() {
+//		Assertions.fail("Ooops test failed");
+    }
+
 }
